6140003abaef9a41473733bd21f440907f5d1188
Triptiskillz <sharmatripti526@gmail.com>
2022-09-27 20:49:52 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.scss";
+var data = require("./MOCK_DATA.json");
 
-function App() {
+export default function App() {
+  const [value, setValue] = useState("");
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onSearch = (searchTerm) => {
+    setValue(searchTerm);
+    console.log("search ", searchTerm);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search-container">
+        <h1>Search</h1>
+        <div className="search-inner">
+          <input
+            type="text"
+            placeholder="Search.."
+            value={value}
+            onChange={onChange}
+          />
+          <i
+            class="fa fa-search"
+            onClick={() => onSearch(value)}
+            aria-hidden="true"
+          ></i>
+        </div>
+
+        <div className="dropdown">
+          {data
+            .filter((item) => {
+              const searchTerm = value.toLowerCase();
+              const fullName = item.full_name.toLowerCase();
+
+              return (
+                searchTerm &&
+                fullName.startsWith(searchTerm) &&
+                fullName !== searchTerm
+              );
+            })
+            .slice(0, 10)
+            .map((item) => (
+              <div
+                onClick={() => onSearch(item.full_name)}
+                className="dropdown-row"
+                key={item.full_name}
+              >
+                <div className="name">{item.full_name}</div>
+
+                <div className="overlay">
+                  <button class="btn">View</button>
+                </div>
+                <div className="fafa">
+                  <i class="fa fa-heart"></i>
+                </div>
+              </div>
+            ))}
+        </div>
+
+        <div className="flex-container">
+          {data
+            .filter((items) => {
+              if (value == "") {
+                return items;
+              } else if (
+                items.full_name.toLowerCase().includes(value.toLowerCase())
+              ) {
+                return items;
+              }
+            })
+            .map((items, key) => {
+              return (
+                <div class="card">
+                  <i class="fa fa-heart"></i>
+                  <img src={items.image} alt="John" />
+                  <h> {items.full_name}</h>
+                  <p class="title">
+                    {items.phone}
+                    <br />
+                    {items.email}
+                  </p>
+                  <p>{items.address}</p>
+                  <p>
+                    <button>View</button>
+                  </p>
+                </div>
+              );
+            })}
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
